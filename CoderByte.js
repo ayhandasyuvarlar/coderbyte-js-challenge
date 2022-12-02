c6691666c383423c13416caf1d4aef1e8f100ba5
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-12-02 14:16:40 +0300

    three numbers

--- CoderByte.js
+++ CoderByte.js
@@ -109,3 +109,111 @@ function CheckNums(num1, num2) {
   }
 }
 //console.log(CheckNums(3, 122))
+
+
+function ArrayChallenge(strArr) {
+  let strOne = strArr[0]
+  let strTwo = strArr[1]
+
+  let singleStrings = strTwo.split(',')
+  let answerWords = " "
+  singleStrings.map((firstWords) => {
+    let splitMainWordArray = strOne.split(firstWords)
+    if (splitMainWordArray.length > 0) {
+      splitMainWordArray.map(word => {
+        let joinedWord = firstWords + word
+        let reverseWord = [joinedWord].reverse().toString()
+        if (joinedWord === strOne || reverseWord === strOne) {
+          answerWords = "" + firstWords + "," + word + ""
+        } else {
+          return;
+        }
+      })
+    }
+  })
+
+  return answerWords
+}
+
+// keep this function call here 
+console.log(ArrayChallenge(readline()));
+
+
+function StringChallenge(str) {
+  let reqex = /[^\w+$+]/
+  return str.split(reqex).map((item, index) => index !== 0 ? item.charAt(0).toUpperCase() + item.slice(1) : item).join('')
+
+}
+
+// keep this function call here 
+//console.log(StringChallenge(readline()));
+
+
+
+function StringChallenge(str) {  //three numbers
+  const words=str.split(' ');
+  let isFinish =false;
+  let index=0;
+  let response=false;
+
+  while((index<words.length && !isFinish)){
+    //3 sayÄ± bir aradamÄ± kontrolÃ¼
+    const areThreeNum=!!words[index].match(/\d{3}/g);
+    const digits=words[index].replace(/\D/g, "");
+
+    const hasThreeDigits=digits.length===3;
+
+    const hasUniqueDigits=digits.replace(/(.)(?=.*\1)/g, "").length===3;
+
+    if(areThreeNum || !hasThreeDigits || !hasUniqueDigits){
+      isFinish=true;
+    }
+    index++;
+  }
+
+  if(!isFinish){
+    response=true;
+  }
+
+  // code goes here  
+  return response; 
+
+}
+   
+// keep this function call here 
+//console.log(StringChallenge(readline()));  
+
+function MathChallenge(num1,num2) { 
+  for(let i = num1 ; i>0; i--){
+    if(num1%i== 0 && num2%i==0){
+      return i
+    }
+  }
+}
+
+// keep this function call here 
+//console.log(MathChallenge(readline()));
+
+
+function ArrayChallenge(arr) {     //[1 , 2  , 3  , 4 , 5 , 6]
+  let arrLength = arr.length
+  let temp = new Array(arrLength);
+  let k = 0
+  let index = arr[0]
+  for (let i = index; i < arrLength; i++) {
+    temp[k] = arr[i]
+    k++
+  }
+  for (let i = 0; i < index; i++) {
+    temp[k] = arr[i]
+    k++
+  }
+
+  for (let i = 0; i <arrLength; i++) {
+    temp[i]
+  }
+  return temp.join('')
+}
+
+// keep this function call here 
+//console.log(ArrayChallenge(readline()));
